fac5525511f0018f06cfdf087feb52a6b856dd9e
dsouzagodfrey <43429997+dsouzagodfrey@users.noreply.github.com>
2020-12-25 12:17:50 +0000

    save working 18 (#12)

--- Spark DataFrames/Dates_and_Timestamps.scala
+++ Spark DataFrames/Dates_and_Timestamps.scala
@@ -9,18 +9,18 @@ val spark = SparkSession.builder().getOrCreate()
 val df = spark.read.option("header","true").option("inferSchema","true").csv("CitiGroup2006_2008")
 
 // Show Schema
-df.printSchema()
+df.show()
 
 // Lot's of options here
 // http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.sql.functions$@add_months(startDate:org.apache.spark.sql.Column,numMonths:Int):org.apache.spark.sql.Column
 
-df.select(month(df("Date"))).show()
+//df.select(month(df("Date"))).show()
 
 df.select(year(df("Date"))).show()
 
 // Practical Example
-val df2 = df.withColumn("Year",year(df("Date")))
+//val df2 = df.withColumn("Year",year(df("Date")))
 
 // Mean per Year, notice large 2008 drop!
-val dfavgs = df2.groupBy("Year").mean()
-dfavgs.select($"Year",$"avg(Close)").show()
+//val dfavgs = df2.groupBy("Year").mean()
+//dfavgs.select($"Year",$"avg(Close)").show()
--- Spark DataFrames/GroupBy_and_Agg.scala
+++ Spark DataFrames/GroupBy_and_Agg.scala
@@ -19,27 +19,27 @@ df.show()
 df.groupBy("Company")
 
 // Mean
-df.groupBy("Company").mean().show()
+//df.groupBy("Company").mean().show()
 // Count
-df.groupBy("Company").count().show()
+//df.groupBy("Company").count().show()
 // Max
-df.groupBy("Company").max().show()
+//df.groupBy("Company").max().show()
 // Min
-df.groupBy("Company").min().show()
+//df.groupBy("Company").min().show()
 // Sum
 df.groupBy("Company").sum().show()
 
 // Other Aggregate Functions
 // http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.sql.functions$
-df.select(countDistinct("Sales")).show() //approxCountDistinct
-df.select(sumDistinct("Sales")).show()
-df.select(variance("Sales")).show()
-df.select(stddev("Sales")).show() //avg,max,min,sum,stddev
-df.select(collect_set("Sales")).show()
+//df.select(countDistinct("Sales")).show() //approxCountDistinct
+//df.select(sumDistinct("Sales")).show()
+//df.select(variance("Sales")).show()
+//df.select(stddev("Sales")).show() //avg,max,min,sum,stddev
+//df.select(collect_set("Sales")).show()
 
 // OrderBy
 // Ascending
-df.orderBy("Sales").show()
+//df.orderBy("Sales").show()
 
 // Descending
-df.orderBy($"Sales".desc).show()
+//df.orderBy($"Sales".desc).show()
